ff460b8ec0ce5cb1aae60060295fdc41dd7e55f0
manue1 <manub1312@googlemail.com>
2014-01-18 12:19:02 +0100

    Added 2 to 1 MUX.

---
+++ 2-1-multiplexer.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL:
+
+entity Multiplexer_2_to_1 is
+port (	-- Datenleitungen
+		A: in STD_LOGIC; -- Bingang 1
+		B: in STD_LOGIC; -- Eingang 2
+		Y: out STD_LOGIC; --Ausgang
+		-- Steuerleitungen
+		S: in STD_LOGIC);
+end Multiplexer_2_to_1;
+
+architecture Behavior of Multiplexer_2_to_1 is
+begin
+	Y <= 	A when (S = '0') else
+			B when (S = '1');
+end Multiplexer_2_to_1;
