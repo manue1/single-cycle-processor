463cdab0105f4296859d5c40ccad1c1e84649582
Hidayat Halim <misterpy@gmail.com>
2014-01-24 15:35:37 +0100

    Edited comment and removed the unneeded behavioural

--- 4-1-multiplexer.vhd
+++ 4-1-multiplexer.vhd
@@ -1,9 +1,9 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL:
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity Multiplexer_4_to_1 is
 port (	-- Datenleitungen
-		A: in STD_LOGIC; -- Bingang 1
+		A: in STD_LOGIC; -- Eingang 1
 		B: in STD_LOGIC; -- Eingang 2
 		C: in STD_LOGIC; -- Eingang 3
 		D: in STD_LOGIC; -- Eingang 4
@@ -19,28 +19,6 @@ begin
 			C when (S = "10") else
 			D when (S = "11");
 end Behavior_When;
-
-architecture Behavior_With of Multiplexer_4_to_1 is
-begin
-	with S select
-		Y <= 	A when "00",
-				B when "01",
-				C when "10",
-				D when "11";
-end Behavior_With;
-
-architecture Behavior_Case of Multiplexer_4_to_1 is
-begin
-	process (S, A, B, C, D)
-	begin
-		case S is
-			when "00" => 	Y <= A;
-			when "01" => 	Y <= B;
-			when "10" => 	Y <= C;
-			when "11" => 	Y <= D;
-		end case;
-	end process;
-end Behavior_Case;
 
 architecture Behavior_If of Multiplexer_4_to_1 is
 begin
