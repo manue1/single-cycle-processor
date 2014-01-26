ec78760f8faf2158bfa19feb516326ac4339a777
Hidayat Halim <misterpy@gmail.com>
2014-01-26 03:07:32 +0100

    removed unused behaviour

--- 1-3-demultiplexer.vhd
+++ 1-3-demultiplexer.vhd
@@ -10,48 +10,11 @@ port (	-- Datenleitungen
 		S: in STD_LOGIC_VECTOR (1 downto 0));
 end Demultiplexer;
 
-architecture Behavior_When of Demultiplexer is
-begin
-	Q <= 	('Z', 'Z', A) when (S = "00") else
-			('Z', A, 'Z') when (S = "01") else
-			(A, 'Z', 'Z') when (S = "10") else
-			('Z', 'Z', 'Z');
-end Behavior_When;
-
-architecture Behavior_With of Demultiplexer is
+architecture Behavior of Demultiplexer is
 begin
 	with S select
 		Q <= 	('Z', 'Z', A) when "00",
 				('Z', A, 'Z') when "01",
 				(A, 'Z', 'Z') when "10",
 				('Z', 'Z', 'Z') when others;
-end Behavior_With;
-
-architecture Behavior_Case of Demultiplexer is
-begin
-	process (S, A)
-	begin
-		case S is
-			when "00" => Q <= ('Z', 'Z', A);
-			when "01" => Q <= ('Z', A, 'Z');
-			when "10" => Q <= (A, 'Z', 'Z');
-			when others => Q <= ('Z', 'Z', 'Z');
-		end case;
-	end process;
-end Behavior_Case;
-
-architecture Behavior_If of Demultiplexer is
-begin
-	process (S, A)
-	begin
-		if (S = "00") then
-			Q <= ('Z', 'Z', A);
-		elsif (S = "01") then
-			Q <= ('Z', A, 'Z');
-		elsif (S = "10") then
-			Q <= (A, 'Z', 'Z');
-		else
-			Q <= ('Z', 'Z', 'Z');
-		end if;
-	end process;
-end Behavior_If;
+end Behavior;
--- 4-1-multiplexer.vhd
+++ 4-1-multiplexer.vhd
@@ -12,26 +12,10 @@ port (	-- Datenleitungen
 		S: in STD_LOGIC_VECTOR (1 downto 0));
 end Multiplexer_4_to_1;
 
-architecture Behavior_When of Multiplexer_4_to_1 is
+architecture Behavior of Multiplexer_4_to_1 is
 begin
 	Y <= 	A when (S = "00") else
 			B when (S = "01") else
 			C when (S = "10") else
 			D when (S = "11");
-end Behavior_When;
-
-architecture Behavior_If of Multiplexer_4_to_1 is
-begin
-	process (S, A, B, C, D)
-	begin
-		if (S = "00") then
-			Y <= A;
-		elsif (S = "01") then
-			Y <= B;
-		elsif (S = "10") then
-			Y <= C;
-		elsif (S = "11") then
-			Y <= D;
-		end if;
-	end process;
-end Behavior_If;
+end Behavior;
